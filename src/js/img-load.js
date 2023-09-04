44be3f5b82e45461fa09fe3f1c3acff5903e4a12
Nick Karch <nickkarch@Nicks-MacBook-Pro.local>
2023-09-04 03:15:46 -0400

    Revamp projects and modal stuff

--- src/js/img-load.js
+++ src/js/img-load.js
@@ -1,26 +1,42 @@
 export default () => {
+    /// Projects
     const projects = document.querySelectorAll(".project");
 
-    projects.forEach((project) => {
-        const imgTrigger = project.querySelector(".img-trigger");
-        imgTrigger.classList.add("loading");
+    function handleImageLoad(el) {
+        el.classList.add("loaded");
+        el.classList.remove("loading");
+        el.removeEventListener("load", handleImageLoad);
+    }
 
-        const img = imgTrigger.querySelector("img");
+    projects.forEach((project) => {
+        // Must apply classes to imgLink instead of img
+        // in order for ::after element (loading state) to work
+        const imgLink = project.querySelector(".img-link");
+        imgLink.classList.add("loading");
+
+        const img = imgLink.querySelector("img");
 
         if (img.naturalHeight > 0) {
-            img.parentElement.classList.add("loaded");
-
-            setTimeout(() => {
-                img.parentElement.classList.remove("loading");
-            }, 1000);
+            handleImageLoad(imgLink);
         } else {
-            img.addEventListener("load", (e) => {
-                const imgParent = e.target.parentElement;
-                imgParent.classList.add("loaded");
+            img.addEventListener("load", () => {
+                handleImageLoad(imgLink);
+            });
+        }
+    });
 
-                setTimeout(() => {
-                    imgParent.classList.remove("loading");
-                }, 1000);
+    /// Tech Stack:
+    const techStackImgs = document.querySelectorAll(".tech-stack img");
+
+    techStackImgs.forEach((img) => {
+        img.classList.add("loading");
+
+        if (img.naturalHeight > 0) {
+            // Image is already loaded:
+            handleImageLoad(img);
+        } else {
+            img.addEventListener("load", () => {
+                handleImageLoad(img);
             });
         }
     });
--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,38 +1,144 @@
+import { v4 as uuidv4 } from "uuid";
+
 export default () => {
-    function getScrollbarWidth() {
-        return window.innerWidth - document.documentElement.clientWidth;
-    }
+    const projectsSection = document.querySelector(".projects");
+    const projects = projectsSection.querySelectorAll(".project");
 
     let scrollbarWidth = getScrollbarWidth();
+    const r = document.querySelector(":root");
+    r.style.setProperty("--scrollbar-width", scrollbarWidth + "px");
 
-    const projects = document.querySelectorAll(".project");
-    const modal = document.getElementById("img-modal");
-    const modalImg = document.getElementById("modal-img");
-    const modalClose = document.getElementById("modal-close");
+    const MODAL_BUTTONS = [
+        { label: "Close", slug: "close", innerText: "×", click: closeModal },
+        {
+            label: "Previous Project",
+            slug: "prev",
+            innerText: "Prev",
+            hidden: true,
+            click: changeProject,
+        },
+        {
+            label: "Next Project",
+            slug: "next",
+            innerText: "Next",
+            hidden: true,
+            click: changeProject,
+        },
+    ];
 
-    projects.forEach((project) => {
-        const imgTrigger = project.querySelector(".img-trigger");
-        const img = imgTrigger.querySelector("img");
+    function generateModalButtons() {
+        const buttonElements = [];
 
-        imgTrigger.addEventListener("click", (e) => {
-            e.preventDefault();
-            modalImg.src = img.dataset.src;
-            modalImg.srcset = img.dataset.srcset;
-            modal.style.display = "block";
-            modal.style.position = "fixed";
+        MODAL_BUTTONS.forEach((buttonData) => {
+            const button = document.createElement("button");
+            button.type = "button";
+            button.className = buttonData.slug;
+            button.id = `modal-${buttonData.slug}`;
+            button.ariaLabel = buttonData.label;
+            button.addEventListener("click", (e) => buttonData.click(buttonData.slug));
 
-            if (window.matchMedia("(hover: hover)").matches) {
-                document.body.style.overflow = "hidden";
-                document.body.style.paddingRight = scrollbarWidth + "px";
+            if (buttonData.innerText) {
+                const span = document.createElement("span");
+                span.innerText = buttonData.innerText;
+
+                if (buttonData.hidden) {
+                    span.hidden = true;
+                }
+
+                button.appendChild(span);
             }
+
+            buttonElements.push(button);
         });
-    });
 
-    if (modal) modal.onclick = (e) => closeModal(e);
-    if (modalClose) modalClose.onclick = (e) => closeModal(e);
+        return buttonElements;
+    }
 
-    function closeModal(e) {
-        if (e.target.id == "modal-img") return;
+    function generateImgMarkup(project) {
+        let img = new Image();
+
+        img.srcset = project.dataset.srcset;
+        img.src = project.dataset.src;
+        img.className = "modal-content";
+        img.Id = "modal-img";
+        img.alt = null;
+
+        return img;
+    }
+
+    function handleModalKeyDown({ modal, e }) {
+        switch (e.key) {
+            case "Tab": {
+                if (e.target.id === "modal-close" && e.shiftKey) {
+                    e.preventDefault();
+                    modal.querySelector("#modal-next")?.focus();
+                } else if (e.target.id === "modal-next" && !e.shiftKey) {
+                    e.preventDefault();
+                    modal.querySelector("#modal-close")?.focus();
+                }
+                break;
+            }
+            case "Escape": {
+                closeModal(modal);
+                break;
+            }
+            case "ArrowLeft": {
+                e.preventDefault();
+                changeProject("prev");
+                break;
+            }
+            case "ArrowRight": {
+                e.preventDefault();
+                changeProject("next");
+                break;
+            }
+        }
+    }
+
+    // In root scope so functions can access same instance
+    let keydownCallback;
+
+    function generateModalMarkup(project) {
+        const modal = document.createElement("div");
+        modal.id = "img-modal";
+        modal.className = "modal";
+        modal.dataset.projectSlug = project.dataset.projectSlug;
+
+        const modalButtons = generateModalButtons();
+        modalButtons.forEach((button) => {
+            modal.appendChild(button);
+        });
+
+        const loading = document.createElement("div");
+        loading.classList.add("loading-spinner");
+        modal.prepend(loading);
+
+        const img = project.querySelector("img");
+        const imgElement = generateImgMarkup(project);
+
+        imgElement.onload = () => {
+            modal.appendChild(imgElement);
+            loading.remove();
+        };
+
+        keydownCallback = (e) => handleModalKeyDown({ modal, e });
+        modal.addEventListener("keydown", keydownCallback);
+
+        return modal;
+    }
+
+    function getScrollbarWidth() {
+        const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
+        return scrollBarWidth;
+    }
+
+    function closeModal() {
+        const modal = document.querySelector("#img-modal");
+        const currentSlug = modal.dataset.projectSlug;
+
+        if (keydownCallback) {
+            modal.removeEventListener("keydown", keydownCallback);
+        }
 
         modal.style.opacity = "0";
 
@@ -42,10 +148,98 @@ export default () => {
                 document.body.style.paddingRight = 0;
             }
 
-            modal.style.display = "none";
-            modal.style.opacity = "";
-        }, 250);
+            modal.remove();
+
+            // Return focus to corresponding project img-link
+            document
+                .querySelector(`.project[data-project-slug="${currentSlug}"] .img-link`)
+                ?.focus();
+        }, 250); // Match CSS opacity transition
     }
+
+    function getAdjacentProject({ direction, currentProject }) {
+        // Get project by direction and current slug
+        let newProject = null;
+        const projects = document.querySelectorAll(`.project`);
+        const projectsArray = Array.from(projects);
+        const currentIndex = projectsArray.indexOf(currentProject);
+
+        if (direction === "next") {
+            if (currentIndex < projectsArray.length - 1) {
+                newProject = projectsArray[currentIndex + 1];
+            } else {
+                newProject = projectsArray[0];
+            }
+        } else if (direction === "prev") {
+            if (currentIndex > 0) {
+                newProject = projectsArray[currentIndex - 1];
+            } else {
+                newProject = projectsArray[projectsArray.length - 1];
+            }
+        }
+
+        return newProject;
+    }
+
+    function changeProject(direction) {
+        const modal = document.querySelector("#img-modal");
+
+        const currentSlug = modal.dataset.projectSlug;
+        const currentProject = document.querySelector(
+            `.project[data-project-slug="${currentSlug}"]`
+        );
+
+        const newProject = getAdjacentProject({ direction, currentProject });
+
+        if (newProject) {
+            modal.dataset.projectSlug = newProject.dataset.projectSlug;
+            const loading = document.createElement("div");
+            loading.classList.add("loading-spinner");
+            modal.prepend(loading);
+            modal.querySelector(".modal-content").remove();
+
+            const imgElement = generateImgMarkup(newProject);
+
+            imgElement.onload = () => {
+                modal.appendChild(imgElement);
+                loading.remove();
+            };
+        }
+    }
+
+    function showModal(project) {
+        const modal = generateModalMarkup(project);
+
+        projectsSection.appendChild(modal);
+
+        modal.querySelector("#modal-close").focus();
+
+        if (window.matchMedia("(hover: hover)").matches) {
+            document.body.style.overflow = "hidden";
+            document.body.style.paddingRight = scrollbarWidth + "px";
+        }
+    }
+
+    function generateProjectSlug(name) {
+        return name ? name.toLowerCase().replace(" ", "-").replace(".", "-") : uuidv4();
+    }
+
+    projects.forEach((project) => {
+        const name = project.querySelector(".h")?.innerText;
+        // Generate slug for modal to reference
+        const slug = generateProjectSlug(name);
+
+        if (slug) {
+            const imgLink = project.querySelector(".img-link");
+
+            project.setAttribute("data-project-slug", slug);
+
+            imgLink.addEventListener("click", (e) => {
+                e.preventDefault();
+                showModal(project);
+            });
+        }
+    });
 
     window.addEventListener("resize", () => {
         scrollbarWidth = getScrollbarWidth();
